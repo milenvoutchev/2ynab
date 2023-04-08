b92b37c382d6ea5ce1a7128f12b5c52d891487ee
Sebastian Rose <sebastian.rose@mailbox.org>
2023-04-08 17:37:45 +0200

    Remove obsolete code

--- server.js
+++ server.js
@@ -1,21 +1,3 @@
-const argv = require('yargs').argv;
-const ConverterFactory = require('./src/ConverterFactory');
-
-let errorMessages = [];
-
-
-let strategy = argv['type'] || argv['_'][0];
-
-if (!strategy) {
-    errorMessages.push('Could not match convert strategy by file name. Please specify it manually: [--type=]' + ConverterFactory.getStrategies().join('|'));
-}
-
-if (errorMessages.length) {
-    // eslint-disable-next-line no-console
-    console.error(errorMessages.join("\r\n"));
-    return 0;
-}
-
 const app = require('./src/server');
 const port = 3000;
 app.listen(port, () => {
